d6a85df345747781c438903a52404ab4947ac8d1
Paulo Basilio <paulolorenzobasilio@yahoo.com>
2020-01-28 20:30:18 +0800

    added product test

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,18 +1,97 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
+import { Repository, getRepository } from 'typeorm';
+import { Product } from './product.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { SellerIdParams } from './params/SellerIdParams';
+import { CreateProductDto } from './dto/CreateProductDto';
+import { UpdateProductDto } from './dto/UpdateProductDto';
+import { SellerParams } from './params/SellerParams';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let repo: Repository<Product>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        {
+          provide: getRepositoryToken(Product),
+          useClass: Repository
+        }
+      ],
     }).compile();
 
     service = module.get<ProductService>(ProductService);
+    repo = module.get<Repository<Product>>(getRepositoryToken(Product))
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+    it('should return array of product', async () => {
+      const testProduct: Product = {
+        id: 1,
+        name: "productName",
+        description: "lorem ipsum",
+        sellerId: 1,
+        seller: null
+      }
+
+      const sellerIdParams: SellerIdParams = {
+        sellerId: 1
+      }
+
+      jest.spyOn(repo, 'find').mockResolvedValueOnce([testProduct])
+      expect(await service.findAll(sellerIdParams)).toEqual([testProduct])
+    })
+  })
+
+  describe('create', () => {
+    it('should insert product data', async () => {
+      const sellerIdParams: SellerIdParams = {
+        sellerId: 1
+      }
+
+      const createProductDto: CreateProductDto = {
+        name: "productName",
+        description: "lorem ipsum",
+      }
+
+      jest.spyOn(repo, 'insert').mockImplementation(() => undefined)
+      expect(await service.create(sellerIdParams, createProductDto)).toBe(undefined)
+    })
+  })
+
+  describe('update', () => {
+    it('should update product', async () => {
+      const sellerParams: SellerParams = {
+        sellerId: 1,
+        id: 1
+      }
+
+      const updateProductDto: UpdateProductDto = {
+        name: "productName",
+        description: "lorem ipsum"
+      }
+
+      jest.spyOn(repo, 'update').mockImplementation(() => undefined)
+      expect(await service.update(sellerParams, updateProductDto)).toBe(undefined)
+    })
+  })
+
+  describe('delete', () => {
+    it('should remove product', async () => {
+      const sellerParams: SellerParams = {
+        sellerId: 1,
+        id: 1
+      }
+
+      jest.spyOn(repo, 'delete').mockImplementation(() => undefined)
+      expect(await service.delete(sellerParams)).toBe(undefined)
+    })
+  })
 });
--- src/seller/seller.service.spec.ts
+++ src/seller/seller.service.spec.ts
@@ -1,18 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SellerService } from './seller.service';
+import { Repository, getRepository } from 'typeorm';
+import { Seller } from './seller.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateSellerDto } from './dto/CreateSellerDto';
+import { IdParams } from './params/IdParams';
+import { UpdateSellerDto } from './dto/UpdateSellerDto';
 
-describe('SellerService', () => {
-  let service: SellerService;
+describe('Seller Service', () => {
+  let service: SellerService
+  let repo: Repository<Seller>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SellerService],
+      providers: [
+        SellerService,
+        {
+          provide: getRepositoryToken(Seller),
+          useClass: Repository
+        }
+      ]
     }).compile();
 
-    service = module.get<SellerService>(SellerService);
+    service = module.get<SellerService>(SellerService)
+    repo = module.get<Repository<Seller>>(getRepositoryToken(Seller))
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+    it('should return an array of seller', async () => {
+      const testSeller: Seller = {
+        id: 1,
+        name: "test1",
+        products: []
+      }
+      jest.spyOn(repo, 'find').mockResolvedValueOnce([testSeller])
+      expect(await service.findAll()).toEqual([testSeller])
+    })
+  })
+
+  describe('create', () => {
+    it('should insert seller data', async () => {
+      const createSellerDto: CreateSellerDto = {
+        name: "testProduct"
+      }
+
+      jest.spyOn(repo, 'insert').mockImplementation(() => undefined)
+      expect(await service.create(createSellerDto)).toBe(undefined)
+    })
+  })
+
+  describe('update', () => {
+    it('should update seller', async () => {
+      const idParams: IdParams = {
+        id: 1
+      }
+      const updateSellerDto: UpdateSellerDto = {
+        name: "updateProduct"
+      }
+      jest.spyOn(repo, 'update').mockImplementation(() => undefined)
+      expect(await service.update(idParams, updateSellerDto)).toBe(undefined)
+    })
+  })
+
+
+  describe('delete', () => {
+    it('should remove seller', async () => {
+      const idParams: IdParams = {
+        id: 1
+      }
+
+      jest.spyOn(repo, 'delete').mockImplementation(() => undefined)
+      expect(await service.delete(idParams)).toBe(undefined)
+    })
+  })
 });
